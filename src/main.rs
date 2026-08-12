851404c566c1e0954ee22cdfdf06fccf1a5f13b4
Buddy <btetnowski@icloud.com>
2026-08-11 21:32:59 -0500

    Simplified code

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,20 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    if request_line == "GET /fun HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) =
+        if request_line == "GET /fun HTTP/1.1" {
+            ("HTTP/1.1 404 NOT FOUND", "hello.html")
+        } else {
+            ("HTTP/1.1 200 OK", "404.html")
+        };
+    if request_line.starts_with("GET /toby-fox.gif") {
+        let image = std::fs::read("toby-fox.gif").unwrap();
+        stream.write_all(&image).unwrap();
     }
-    else{
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        if request_line.starts_with("GET /toby-fox.gif") {
-            let image = std::fs::read("toby-fox.gif").unwrap();
-            stream.write_all(&image).unwrap();
-        }
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-    }
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
